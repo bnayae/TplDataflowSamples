a4a9a2abfc2de0ae5998c4284e42072cb3e992c7
bnayae <Bnayanet@hotmail.com>
2019-06-25 15:34:17 +0300

    exercise

---
+++ ImageManipFlow/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+/*  Nugets (beta)
+
+SixLabors.ImageSharp
+SixLabors.Fonts
+SixLabors.Shapes.Text
+SixLabors.ImageSharp.Drawing
+
+*/
+namespace ImageManipFlow
+{
+    class Program
+    {
+
+        private const string DOGS = "https://source.unsplash.com/1000x1000/?dog/";
+        private const string CATS = "https://source.unsplash.com/1000x1000/?cat/";
+
+        static async Task Main(string[] args)
+        {
+            using (var http = new HttpClient())
+            {
+                var buffer = await http.GetByteArrayAsync(DOGS).ConfigureAwait(false);
+                using (Image<Rgba32> imageProcessor = Image.Load(buffer))
+                using (var srm = new FileStream("img.jpg", FileMode.Create,
+                    FileAccess.Write, FileShare.None, 4096, FileOptions.Asynchronous))
+                {
+                    imageProcessor.Mutate(x => x
+                                        .OilPaint()
+                                        .Grayscale()
+                                        .GaussianBlur()
+                                        .Pixelate(20));
+                    imageProcessor.SaveAsJpeg(srm);
+                }
+            }
+
+            Console.WriteLine("Done");
+        }
+    }
+}
